19a650067cb6fd7b4bdaff710326c84d2a2f2166
Zarahji <chukolfatima@gmail.com>
2025-02-19 20:38:55 +0100

    initial file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+"import UserProfile"
 
 import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
