e021a0b9ba922d4a8965eb99e725d9e0feb04da8
Zarahji <chukolfatima@gmail.com>
2025-02-19 20:28:15 +0100

    initial file

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,3 +1,14 @@
 <footer>
   <p>© 2023 City Lovers</p>
 </footer>
+import React from 'react';
+
+function Footer() {
+  return (
+    <main>
+      <p>This is the main content of the app.</p>
+    </main>
+  );
+}
+
+export default Footer;
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,3 +1,14 @@
 <main>
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <p>This is the main content of the app.</p>
+    </main>
+  );
+}
+
+export default MainContent;
