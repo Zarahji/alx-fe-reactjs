bfb083277f38c7dd8a9c270af2aecde20aad9b9f
Zarahji <chukolfatima@gmail.com>
2025-03-22 08:17:50 +0100

    tailwind.config.js fixed

--- tailwind-react-integration/src/components/tailwind.config.js
+++ tailwind-react-integration/src/components/tailwind.config.js
@@ -1,28 +1,10 @@
 module.exports = {
-  purge: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
     "./public/index.html",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
-module.exports = {
-  purge: [
-    './src/*.html',
-    './src/*.js',
-    './src//*.jsx',
-    './src/*.ts',
-    './src/*.tsx',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
